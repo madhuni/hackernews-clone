b26609b4e993cbc6f14eb830b8873e996dc1b07f
Kanishka Mohan Madhuni <madhuni@Kanishkas-MacBook-Air.local>
2020-06-20 23:50:29 +0530

    new(api.service): add new service to interact with APIs
    Add a function to fetch the news from the server.

---
+++ src/services/api.service.js
@@ -0,0 +1,19 @@
+/**
+ * This function will fetch the NEWS provided the page number.
+ * @param {*} pageNumber - Page number
+ * @returns {Promise<any>}
+ */
+const getNews = (pageNumber = 1) => {
+  const url = `http://hn.algolia.com/api/v1/search?page=${pageNumber}`;
+  return fetch(url)
+    .then((res) => res.json())
+    .then(({ hits }) => {
+      const transformedData = hits.map((hit) => ({
+        ...hit,
+        hide: false,
+      }));
+      return Promise.resolve(transformedData);
+    });
+};
+
+export default getNews;
