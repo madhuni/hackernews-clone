e279cec3ee80d171bcf69f39223721faa1b41c85
Kanishka Mohan Madhuni <madhuni@Kanishkas-MacBook-Air.local>
2020-06-20 23:57:27 +0530

    new(NewsDetails): add new NewsDetails component
    This compoent will render the news details in a specific manner
    provided in designs. This component will combine the below properties together:
    1. Title of the news
    2. URL of the news
    3. Author of the news
    4. Date of the news

---
+++ src/components/NewsDetails/NewsDetails.component.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { getShortURL, getFormattedDate } from '../../services/utility.service';
+
+import './NewsDetails.component.scss';
+
+const NewsDetails = ({ title, url, author, createdAt }) => {
+  const urlString = url ? `(${getShortURL(url)})` : '';
+  return (
+    <>
+      <span>{title}</span>
+      <a className="t-caption color-light" href={url}>
+        {' '}
+        {urlString}
+      </a>
+      <span>
+        <span className="t-caption color-light"> by</span> {author}
+      </span>
+      <span>
+        <span className="t-caption color-light"> on</span>{' '}
+        {getFormattedDate(createdAt)}
+      </span>
+    </>
+  );
+};
+
+NewsDetails.propTypes = {
+  title: PropTypes.string,
+  url: PropTypes.string,
+  author: PropTypes.string,
+  createdAt: PropTypes.string,
+};
+
+NewsDetails.defaultProps = {
+  title: '',
+  url: '',
+  author: '',
+  createdAt: '',
+};
+
+export default NewsDetails;
