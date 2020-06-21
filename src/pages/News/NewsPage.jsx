7c7aa61481e3063751f5ae46a49c539d93c1b709
Kanishka Mohan Madhuni <madhuni@Kanishkas-MacBook-Air.local>
2020-06-21 13:24:56 +0530

    refactor(NewsPage): add logic to consume Redux store

--- src/pages/News/NewsPage.jsx
+++ src/pages/News/NewsPage.jsx
@@ -1,83 +1,69 @@
 /* eslint-disable no-console */
 import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import NewsCard from '../../components/NewsCard/NewsCard.component';
 
-import { getNews, updateModifiedEntries } from '../../services/api.service';
+import { getNews } from '../../services/api.service';
+import {
+  getNewsResponse,
+  upvoteNewsItem,
+  hideNewsItem,
+} from '../../store/actions/news.action';
 
 import './NewsPage.scss';
 
 export default function NewsPage() {
   const [loading, setLoading] = useState(true);
   const [noContent, setNoContent] = useState(false);
-  const [news, setNews] = useState([]);
+  const news = useSelector((state) => state.news);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     getNews()
       .then((res) => {
         setLoading(false);
-        if (!res.length) {
-          setNoContent(true);
-        }
-        setNews(res);
+        dispatch(getNewsResponse(res));
       })
       .catch(() => {
         setLoading(false);
-        setNoContent(true);
-        setNews([]);
       });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  useEffect(() => {
+    if (!news.length && !loading) {
+      setNoContent(true);
+    } else {
+      setNoContent(false);
+    }
+  }, [news, loading]);
 
   /**
    * This is a callback function which will trigger the upvote action on a card.
    * @param {*} id - Unique id of the news item
    */
-  const onUpvote = (id) => {
-    const modifiedNews = news.map((item) => {
-      if (item.objectID === id) {
-        const modifiedItem = { ...item, points: item.points + 1 };
-        updateModifiedEntries(modifiedItem);
-        return modifiedItem;
-      }
-      return item;
-    });
-    setNews(modifiedNews);
-  };
+  const onUpvote = (newsItemId) => dispatch(upvoteNewsItem(newsItemId));
 
   /**
    * This is a callback function which will trigger the hide action on a card.
    * @param {*} id - Unique id of the news item
    */
-  const onHideNews = (id) => {
-    const modifiedNews = news
-      .map((item) => {
-        if (item.objectID === id) {
-          const modifiedItem = { ...item, hide: true };
-          updateModifiedEntries(modifiedItem);
-          return modifiedItem;
-        }
-        return item;
-      })
-      .filter((item) => !item.hide);
-
-    setNews(modifiedNews);
-    if (!modifiedNews.length) {
-      setNoContent(true);
-    }
-  };
+  const onHideNews = (newsItemId) => dispatch(hideNewsItem(newsItemId));
 
   return (
     <div className="news container">
       {loading && <div className="loading">Loading News...</div>}
       {noContent && <div className="no-content">No news found!</div>}
-      {news.map((item) => (
-        <NewsCard
-          key={item.objectID}
-          news={item}
-          upvoteHandler={() => onUpvote(item.objectID)}
-          hideNewsHandler={onHideNews}
-        />
-      ))}
+      {!noContent &&
+        news.map((item) => (
+          <NewsCard
+            key={item.objectID}
+            news={item}
+            upvoteHandler={() => onUpvote(item.objectID)}
+            hideNewsHandler={onHideNews}
+          />
+        ))}
     </div>
   );
 }
