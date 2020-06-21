de8ccc612b7f08ee33323b9a1143e9bbfad20d9f
Kanishka Mohan Madhuni <madhuni@Kanishkas-MacBook-Air.local>
2020-06-21 23:28:22 +0530

    refactor(news.reducer): refactor the reducer

--- src/store/reducers/news.reducer.js
+++ src/store/reducers/news.reducer.js
@@ -6,44 +6,34 @@ import {
 import { updateModifiedEntries } from '../../services/api.service';
 
 /* Define the initial state of the app */
-const initialState = {
-  news: [],
-};
+const initialState = [];
 
 /* Setup reducer function for managing news state */
-const news = (state = initialState, action) => {
+export default function newsReducer(state = initialState, action) {
   switch (action.type) {
     case GET_NEWS_RESPONSE:
-      return {
-        news: [...action.response],
-      };
+      return [...action.response];
     case HIDE_NEWS_ITEM:
-      return {
-        news: state.news
-          .map((item) => {
-            if (item.objectID === action.newsItemId) {
-              const modifiedItem = { ...item, hide: true };
-              updateModifiedEntries(modifiedItem); // side-effect to update the localstorage
-              return modifiedItem;
-            }
-            return item;
-          })
-          .filter((item) => !item.hide),
-      };
-    case UPVOTE_NEWS_ITEM:
-      return {
-        news: state.news.map((item) => {
+      return state
+        .map((item) => {
           if (item.objectID === action.newsItemId) {
-            const modifiedItem = { ...item, points: item.points + 1 };
+            const modifiedItem = { ...item, hide: true };
             updateModifiedEntries(modifiedItem); // side-effect to update the localstorage
             return modifiedItem;
           }
           return item;
-        }),
-      };
+        })
+        .filter((item) => !item.hide);
+    case UPVOTE_NEWS_ITEM:
+      return state.news.map((item) => {
+        if (item.objectID === action.newsItemId) {
+          const modifiedItem = { ...item, points: item.points + 1 };
+          updateModifiedEntries(modifiedItem); // side-effect to update the localstorage
+          return modifiedItem;
+        }
+        return item;
+      });
     default:
       return state;
   }
-};
-
-export default news;
+}
