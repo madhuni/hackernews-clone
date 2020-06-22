deea3fedb27d6f68819e6933d2384bf67636e7f3
Kanishka Mohan Madhuni <madhuni@Kanishkas-MacBook-Air.local>
2020-06-22 13:26:07 +0530

    refactor(App): use Pagniator component for pagination and refactor the code

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { PAGE_INCREMENT, PAGE_DECREMENT } from './store/actions/page.action';
 
 import NavBar from './components/NavBar/NavBar.component';
-import PaginationButton from './components/PaginationButton/PaginationButton.component';
+import Paginator from './components/Paginator/Paginator.component';
 import Routes from './routes/routes';
 
 import './App.scss';
@@ -17,30 +17,18 @@ function App() {
   const onIncrementPageNumber = () => dispatch({ type: PAGE_INCREMENT });
   const onDecrementPageNumber = () => dispatch({ type: PAGE_DECREMENT });
 
-  /**
-   * Component which defines the structure of the pagination buttons
-   */
-  const PaginationActions = () => (
-    <section className="pagination-actions">
-      <PaginationButton
-        type="back"
-        clickHandler={onDecrementPageNumber}
-        disabled={currentPage === 1}
-      >
-        Previous
-      </PaginationButton>
-      <span className="divider" />
-      <PaginationButton type="forward" clickHandler={onIncrementPageNumber}>
-        Next
-      </PaginationButton>
-    </section>
-  );
-
   return (
     <div className="App">
       <NavBar />
       <Routes />
-      {currentPage !== null ? <PaginationActions /> : null}
+      {/* {currentPage !== null ? <PaginationActions /> : null} */}
+      {currentPage !== null ? (
+        <Paginator
+          onDecrementHandler={onDecrementPageNumber}
+          onIncrementHandler={onIncrementPageNumber}
+          backDisabled={currentPage === 1}
+        />
+      ) : null}
     </div>
   );
 }
