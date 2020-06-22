9edc60906a6ef51749113a8c3ba6012e3a247061
Kanishka Mohan Madhuni <madhuni@Kanishkas-MacBook-Air.local>
2020-06-22 12:42:44 +0530

    update(routes): update route for the app

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -7,7 +7,8 @@ import NewsPage from '../pages/News/NewsPage';
 export default function Routes() {
   return (
     <Switch>
-      <Route path="/" component={NewsPage} />
+      <Route path="/" exact component={NewsPage} />
+      <Route path="/:id" component={NewsPage} />
     </Switch>
   );
 }
