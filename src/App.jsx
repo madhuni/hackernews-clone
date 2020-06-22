06f52885ec55bee818d66e07cdbc664760fa0318
Kanishka Mohan Madhuni <madhuni@Kanishkas-MacBook-Air.local>
2020-06-22 12:29:05 +0530

    feature: add feature to navigate pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { PAGE_INCREMENT, PAGE_DECREMENT } from './store/actions/page.action';
 
 import NavBar from './components/NavBar/NavBar.component';
 import PaginationButton from './components/PaginationButton/PaginationButton.component';
@@ -7,19 +10,37 @@ import Routes from './routes/routes';
 import './App.scss';
 
 function App() {
+  const currentPage = useSelector((state) => state.currentPage);
+  const dispatch = useDispatch();
+
+  /* Callback function for Page increment/decrement action */
+  const onIncrementPageNumber = () => dispatch({ type: PAGE_INCREMENT });
+  const onDecrementPageNumber = () => dispatch({ type: PAGE_DECREMENT });
+
+  /**
+   * Component which defines the structure of the pagination buttons
+   */
+  const PaginationActions = () => (
+    <section className="pagination-actions">
+      <PaginationButton
+        type="back"
+        clickHandler={onDecrementPageNumber}
+        disabled={currentPage === 1}
+      >
+        Previous
+      </PaginationButton>
+      <span className="divider" />
+      <PaginationButton type="forward" clickHandler={onIncrementPageNumber}>
+        Next
+      </PaginationButton>
+    </section>
+  );
+
   return (
     <div className="App">
       <NavBar />
       <Routes />
-      <section className="pagination-actions">
-        <PaginationButton type="back" clickHandler={() => null}>
-          Previous
-        </PaginationButton>
-        <span className="divider" />
-        <PaginationButton type="forward" clickHandler={() => null}>
-          Next
-        </PaginationButton>
-      </section>
+      {currentPage !== null ? <PaginationActions /> : null}
     </div>
   );
 }
